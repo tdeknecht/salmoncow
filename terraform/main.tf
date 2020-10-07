739faedf9e9fb81da599be2d648cb945177de0d3
tdeknecht <tdeknecht@gmail.com>
2020-10-06 20:24:48 -0500

    adjustements to stack and www

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,6 +81,9 @@ resource "aws_cloudfront_distribution" "salmoncow_s3_distribution" {
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = local.s3_origin_id
     viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
 
     forwarded_values {
       query_string = false
