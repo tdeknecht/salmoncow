3f7baa90660befb6ba24c0defbf7a90161d34c41
tdeknecht <tdeknecht@gmail.com>
2021-10-24 09:12:21 -0500

    moving on to next step: api

--- src/spa/src/constants/cognito.js
+++ src/spa/src/constants/cognito.js
@@ -5,8 +5,8 @@ export const COGNITO_CLIENT_ID = '1kpdb8dcjqpv7i9fhh1859rkbu';
 export const COGNITO_IDENTITY_POOL_ID = 'us-east-1:6fd26082-0bbc-4cb3-8d16-a6b8da347bcc';
 
 // https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html
-export const COGNITO_PUB_JWKS = {"keys":[{"alg":"RS256","e":"AQAB","kid":"yEWksOnjLe/Wovh3ez/fiviDbF4jVcZyHfQy+BR0oy8=","kty":"RSA","n":"qk_Zb2qyfUaOUY-wSpImbHqpEyKsOM5eQYe8d5NLldaWhsJHcdNKYe3hYZc91gK6BXfWTI0EiimT_n1ThwwRF5jOj7agz7euW301ejthKoRcVhJ-L68SN00QKop-9Nl7cEV6e6QLWKS8nrP_nIVdGo8H67PehymzjU-WZzqFRNxnIO5Metj41JXDiME7ycyNMv2cD5GEBnznv_J17UJ7TjItVNHzxO-lx4X1AOT5Y3a45hhfzqzzAd9D-DWgf54WxPeNUACJ8lkbbGkYSw5fTHr4-4V50oiHXN2QQl7P8M5zkT0UKt14CymkNajygEPnyjyDJNwSXDMDTSrNZ7E6zQ","use":"sig"},{"alg":"RS256","e":"AQAB","kid":"1RG8vUJj42i3rbUCTqeKS2CKedJ9QxSUT9N1X83fhEQ=","kty":"RSA","n":"vWzn3qdwbHBRqIV0CGIz7vWwt6Y80Inb57pkPnBYFRtVKC5yp4YOKwZSVGV_3GjcsvIvXr63wwAMETlmm4zFLq3ixC5wZzRbdm5Xd0eVgmQKvajX-b8ViBZTR9em25RBSlBJ9ClhpKCJAbr11ueuUKju4okBwITLGFi_SWhpEloJHWNMsuhVOY62g3BEOqkzxR1FfT_R56uazxZx17bikUZSFmFy7NX8-f64FgWLcvVbL9HaOZ9sBAJuiNtfJRYv3Bg8FfMacdndi6iXyp_f8DDf5aSnGmVEwtGqsrrETy6LKIF4iq2UB3gvFeOrTvJoB1DwhDTrfLrTBGimymH6yQ","use":"sig"}]}
+export const COGNITO_PUB_JWKS = {"keys":[{"alg":"RS256","e":"AQAB","kid":"yEWksOnjLe/Wovh3ez/fiviDbF4jVcZyHfQy+BR0oy8=","kty":"RSA","n":"qk_Zb2qyfUaOUY-wSpImbHqpEyKsOM5eQYe8d5NLldaWhsJHcdNKYe3hYZc91gK6BXfWTI0EiimT_n1ThwwRF5jOj7agz7euW301ejthKoRcVhJ-L68SN00QKop-9Nl7cEV6e6QLWKS8nrP_nIVdGo8H67PehymzjU-WZzqFRNxnIO5Metj41JXDiME7ycyNMv2cD5GEBnznv_J17UJ7TjItVNHzxO-lx4X1AOT5Y3a45hhfzqzzAd9D-DWgf54WxPeNUACJ8lkbbGkYSw5fTHr4-4V50oiHXN2QQl7P8M5zkT0UKt14CymkNajygEPnyjyDJNwSXDMDTSrNZ7E6zQ","use":"sig"},{"alg":"RS256","e":"AQAB","kid":"1RG8vUJj42i3rbUCTqeKS2CKedJ9QxSUT9N1X83fhEQ=","kty":"RSA","n":"vWzn3qdwbHBRqIV0CGIz7vWwt6Y80Inb57pkPnBYFRtVKC5yp4YOKwZSVGV_3GjcsvIvXr63wwAMETlmm4zFLq3ixC5wZzRbdm5Xd0eVgmQKvajX-b8ViBZTR9em25RBSlBJ9ClhpKCJAbr11ueuUKju4okBwITLGFi_SWhpEloJHWNMsuhVOY62g3BEOqkzxR1FfT_R56uazxZx17bikUZSFmFy7NX8-f64FgWLcvVbL9HaOZ9sBAJuiNtfJRYv3Bg8FfMacdndi6iXyp_f8DDf5aSnGmVEwtGqsrrETy6LKIF4iq2UB3gvFeOrTvJoB1DwhDTrfLrTBGimymH6yQ","use":"sig"}]};
 
 
-export const COGNITO_ID_TOKEN = 'cognito_id_token';
-export const COGNITO_ACCESS_TOKEN = 'cognito_access_token';
+export const COGNITO_ID_TOKEN = null;
+export const COGNITO_ACCESS_TOKEN = null;
