80e0846e9c36c41b550255b1b7b76a731c284b27
tdeknecht <tdeknecht@gmail.com>
2021-10-30 17:02:34 -0500

    WIP: a few ignores, removing old js lambda

--- src/terraform/federation/CognitoPreSignUpLambda.js
+++
@@ -1,9 +0,0 @@
-// https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-pre-sign-up.html
-
-exports.handler = function(event, context, callback) {
-  event.response.autoConfirmUser = true;
-  // event.response.autoVerifyEmail = true;
-  // event.response.autoVerifyPhone = true;
-  
-  context.done(null, event);
-}
