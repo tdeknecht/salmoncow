c6e364ed7978de7075c773cefa44337b75701ccd
tdeknecht <tdeknecht@gmail.com>
2022-05-22 11:57:38 -0500

    WIP: working version of demo

--- src/spa/src/App.js
+++ src/spa/src/App.js
@@ -1,9 +1,8 @@
 // Used some of this guide: https://medium.com/technoetics/create-basic-login-forms-using-react-js-hooks-and-bootstrap-2ae36c15e551
 // AWS Cognito guide: https://github.com/aws-amplify/amplify-js/tree/master/packages/amazon-cognito-identity-js#setup
 
-import React, {useState} from 'react';
+import React from 'react';
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
   Link,
@@ -11,7 +10,9 @@ import {
   useLocation,
   Navigate,
   Outlet,
-} from "react-router-dom";
+} from 'react-router-dom';
+
+import Grid from '@mui/material/Grid';
 
 import './App.css';
 import logo from './images/salmoncow.png';
@@ -20,60 +21,178 @@ import Home from './components/Home/Home';
 import Header from './components/Header/Header';
 import LoginForm from './components/LoginForm/LoginForm';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm';
-import PrivateRoute from './utils/PrivateRoute';
-
-import Grid from '@mui/material/Grid';
+import { fakeAuthProvider } from './utils/fakeAuthProvider';
 
 export default function App() {
   return (
-    <Router>
-      <Header />
-      <Grid
-        container
-        spacing={0}
-        direction='column'
-        alignItems='center'
-        style={{ minHeight: '100vh' }}
-      >
-        <img src={logo} className='logo' alt='logo' />
-          <Routes>
-            {/* <Route element={<Header />}> */}
-              <Route path='/register' element={<RegistrationForm />} />
-              <Route path='/login' element={<LoginForm />} />
-              {/* <PrivateRoute path='/' element={<Home />} /> */}
-            {/* </Route> */}
-          </Routes>
-        <p className='footer'>
-          This is a dev site. It's simply a fun <a href='https://github.com/tdeknecht/salmoncow' target='_blank' rel='noopener noreferrer'>side project</a>.
-        </p>
-      </Grid>
-    </Router>
+      <AuthProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path='/register' element={<RegistrationForm />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route
+              path="/protected"
+              element={
+                <RequireAuth>
+                  <ProtectedPage />
+                </RequireAuth>
+              }
+            />
+          </Route>
+        </Routes>
+      </AuthProvider>
   );
 }
 
-// function App() {
-//   return (
-//     <div>
-//       <img src={logo} className='App-logo' alt='logo' />
-//       <Welcome name='Tyler D' />
-//     </div>
-//   );
-// }
-// export default App;
+function Layout() {
+  return (
+    <div>
+      <Header />
+        <Grid
+          container
+          spacing={0}
+          direction='column'
+          alignItems='center'
+          style={{ minHeight: '100vh' }}
+        >
+        <img src={logo} className='logo' alt='logo' />
 
-// (A) function approach to Welcome
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
+        {/* <AuthStatus /> */}
 
-// (B) alternative syntax to above. Useful for defining functions within a function...?
-// const Welcome = (props) => {
-//   return <h1>Hello, {props.name}</h1>;
-// }
+        <ul>
+          <li>
+            <Link to="/">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
 
-// (C) class approach to Welcome
-// class Welcome extends React.Component {
-//   render() {
-//     return <h1>Hello, {this.props.name}</h1>;
-//   }
-// }
+        <Outlet />
+      <p className='footer'>
+          This is a dev site. It's simply a fun <a href='https://github.com/tdeknecht/salmoncow' target='_blank' rel='noopener noreferrer'>side project</a>.
+        </p>
+      </Grid>
+    </div>
+  );
+}
+
+export const AuthContextType = {
+  user : null,
+  signin : (user, callback) => null,
+  signout : (callback) => null,
+}
+
+let AuthContext = React.createContext(
+  AuthContextType.null
+);
+
+function AuthProvider({ children }) {
+  let [user, setUser] = React.useState(null);
+
+  let signin = (newUser, callback) => {
+    return fakeAuthProvider.signin(() => {
+      setUser(newUser);
+      callback();
+    });
+  };
+
+  let signout = (callback) => {
+    return fakeAuthProvider.signout(() => {
+      setUser(null);
+      callback();
+    });
+  };
+
+  let value = { user, signin, signout };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  return React.useContext(AuthContext);
+}
+
+function AuthStatus() {
+  let auth = useAuth();
+  let navigate = useNavigate();
+
+  if (!auth.user) { // if used in Protected, remove this. Assumed already logged in
+    return <p>You are not logged in.</p>;
+  }
+
+  return (
+    <p>
+      Welcome {auth.user}!{' '}
+      <button
+        onClick={() => {
+          auth.signout(() => navigate('/'));
+        }}
+      >
+        Sign out
+      </button>
+    </p>
+  );
+}
+
+function RequireAuth({ children }) {
+  let auth = useAuth();
+  let location = useLocation();
+
+  if (!auth.user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+
+function LoginPage() {
+  let navigate = useNavigate();
+  let location = useLocation();
+  let auth = useAuth();
+
+  let from = location.state?.from?.pathname || '/';
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let formData = new FormData(event.currentTarget);
+    let username = formData.get('username');
+
+    auth.signin(username, () => {
+      // Send them back to the page they tried to visit when they were
+      // redirected to the login page. Use { replace: true } so we don't create
+      // another entry in the history stack for the login page.  This means that
+      // when they get to the protected page and click the back button, they
+      // won't end up back on the login page, which is also really nice for the
+      // user experience.
+      navigate(from, { replace: true });
+    });
+  }
+
+  return (
+    <div>
+      <p>You must log in to view the page at {from}</p>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username: <input name="username" type="text" />
+        </label>{' '}
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+function ProtectedPage() {
+  return <h3><AuthStatus /></h3>;
+}
--- src/spa/src/components/Header/Header.js
+++ src/spa/src/components/Header/Header.js
@@ -33,40 +33,38 @@ function Header(props) {
   };
 
   function renderProfile() {
-    if(location.pathname === '/'){
-      return(
-        <div>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleMenu}
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem>Profile</MenuItem>
-            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-          </Menu>
-        </div>
-      )
-    }
+    return(
+      <div>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleMenu}
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem>Profile</MenuItem>
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
+      </div>
+    )
   }
 
   function handleLogout() {
